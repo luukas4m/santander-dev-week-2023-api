361f40341110f38835390ff410a3e935c9bad07a
João Lucas <88448250+luukas4m@users.noreply.github.com>
2023-10-18 19:02:25 -0300

    Update 1 luukas4m

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -2,7 +2,6 @@ package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
